2c528b1031945e376b18c89704c497403a685711
seongjin605 <qkrtjdwls605@naver.com>
2019-03-05 17:23:16 +0900

    push server

---
+++ blog-backend/src/index.js
@@ -0,0 +1,15 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const api = require('./api');
+
+const app = new Koa();
+const router = Router();
+
+router.use('/api', api.routes()); // api 라우터 적용
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('listening to port 4000');
+});
